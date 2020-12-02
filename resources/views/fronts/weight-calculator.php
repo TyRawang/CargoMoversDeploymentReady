846a557b758f88b83e63541e2c007b843fe48ccf
TyRawang <31902431+TyRawang@users.noreply.github.com>
2020-12-01 18:59:04 -0700

    f
    - added weight-calculator assets
    - change one of the asset to link,

--- resources/views/fronts/weight-calculator.php
+++ resources/views/fronts/weight-calculator.php
@@ -7,7 +7,8 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
     integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog=="
     crossorigin="anonymous" />
-  <link rel="stylesheet" href="./assets/fonts/icon-fonts/flaticon.css">
+  <!-- <link rel="stylesheet" href="./assets/fonts/icon-fonts/flaticon.css"> -->
+  <link rel="stylesheet" href=" {{ url('assets/weight-calculator/fonts/icon-fonts/flaticon.css">
 
   <style type="text/css">
     #root {
